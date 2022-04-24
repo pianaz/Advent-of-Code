3dfaa8364d2d23714e1c93bf04dd141b81aab22f
pianaz <enrico.pianaz@gmail.com>
2022-04-24 11:33:31 +0200

    day 6 part 2 solution

--- AOC/src/day6.cpp
+++ AOC/src/day6.cpp
@@ -48,3 +48,48 @@ void day6_1() {
 
 	std::cout << count << '\n';
 }
+
+void day6_2() {
+	std::ifstream input("input6.txt");
+	std::string line;
+	std::regex input_parse{ R"((off|on|toggle)\s([0-9]+),([0-9]+)\sthrough\s([0-9]+),([0-9]+))" };
+	std::smatch matches;
+	int rowInit = 0;
+	int rowFinal = 0;
+	int colInit = 0;
+	int colFinal = 0;
+	std::vector<std::vector<int>> grid(1000, std::vector<int>(1000));
+	for (int i = 0; i < 1000; i++) {
+		for (int j = 0; j < 1000; j++) {
+			grid[i][j] = 0;
+		}
+	}
+	enum Mode { off, on, toggle };
+	Mode mode = off;
+
+	while (std::getline(input, line)) {
+		std::regex_search(line, matches, input_parse);
+		if (matches[1] == "on") mode = on;
+		if (matches[1] == "off") mode = off;
+		if (matches[1] == "toggle") mode = toggle;
+		rowInit = std::stoi(matches[2]);
+		colInit = std::stoi(matches[3]);
+		rowFinal = std::stoi(matches[4]);
+		colFinal = std::stoi(matches[5]);
+		for (int i = rowInit; i < rowFinal + 1; i++) {
+			for (int j = colInit; j < colFinal + 1; j++) {
+				if (mode == toggle) grid[i][j] += mode;
+				if (mode == on) grid[i][j] += mode;
+				if (mode == off) grid[i][j] = std::max(0, grid[i][j]-1);
+			}
+		}
+	}
+	int count = 0;
+	for (int i = 0; i < 1000; i++) {
+		for (int j = 0; j < 1000; j++) {
+			count += grid[i][j];
+		}
+	}
+
+	std::cout << count << '\n';
+}
--- AOC/src/main.cpp
+++ AOC/src/main.cpp
@@ -6,11 +6,9 @@ void day4_2();
 void day5_1();
 void day5_2();
 void day6_1();
+void day6_2();
 
 
 int main(int argc, char** argv) {
-	//day3();
-	//day4_2();
-	//day5_2();
-	day6_1();
+	day6_2();
 }
